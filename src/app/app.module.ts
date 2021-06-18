ad0322b9214b8c12661745452846d32ef8a777ae
pmorais13 <phmscout@gmail.com>
2021-06-18 04:17:05 -0300

    first structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PlayerComponent } from './features/components/player/player.component';
+import { GenresComponent } from './features/components/genres/genres.component';
+import { MusicListComponent } from './features/components/music-list/music-list.component';
+import { LyricsComponent } from './features/components/lyrics/lyrics.component';
+import { FavoriteSongsComponent } from './features/components/favorite-songs/favorite-songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PlayerComponent,
+    GenresComponent,
+    MusicListComponent,
+    LyricsComponent,
+    FavoriteSongsComponent
   ],
   imports: [
     BrowserModule,
